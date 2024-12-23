bc3c71c51224ffb85e31cb2faea027c218e32456
Mithun Srinavaratnarajah <lnsmirth@googlemail.com>
2024-12-23 22:49:01 +0000

    Refactor visualisation mechanism for plot

--- web/app/circos/page.tsx
+++ web/app/circos/page.tsx
@@ -1,193 +1,210 @@
-"use client"
+"use client";
 
-import { useEffect, useRef, useState } from 'react';
-import * as d3 from 'd3';
-import { Assembly } from '../types/genomes';
-import FileUpload from '../../components/FileUpload';
-import Form from '../../components/Form';
+import { useEffect, useRef, useState } from "react";
+import * as d3 from "d3";
+import { Assembly } from "../types/genomes";
+import FileUpload from "../../components/FileUpload";
+import Form from "../../components/Form";
 
 const CanvasPage = () => {
-  const canvasRef = useRef<HTMLDivElement>(null);
-  const [segments, setSegments] = useState<Assembly[]>([]);
-  const [segmentAnglePadding, setSegmentAnglePadding] = useState(0.02);
-  const [axisLabelFontSize, setAxisLabelFontSize] = useState(6);
-  const [showAxis, setShowAxis] = useState(true);
-  const [segmentInnerRadius, setSegmentInnerRadius] = useState(200);
-  const [segmentOuterRadius, setSegmentOuterRadius] = useState(250);
-  const [showAxisLabels, setShowAxisLabels] = useState(true);
-  const [canvasWidth, setCanvasWidth] = useState(800);
-  const [canvasHeight, setCanvasHeight] = useState(600);
-  const [segmentGridPadding, setSegmentGridPadding] = useState(3);
-  const [tickLength, setTickLength] = useState(3);
-  const [tickTextPadding, setTickTextPadding] = useState(3);
+    const canvasRef = useRef<HTMLDivElement>(null);
+    const [segments, setSegments] = useState<Assembly[]>([]);
+    const [axisLabelFontSize, setAxisLabelFontSize] = useState(10);
+    const [showAxis, setShowAxis] = useState(true);
+    const [segmentGridPadding, setSegmentGridPadding] = useState(3);
+    const [tickLength, setTickLength] = useState(3);
+    const [tickTextPadding, setTickTextPadding] = useState(3);
+    const [segmentInnerRadius, setSegmentInnerRadius] = useState(200);
+    const [segmentOuterRadius, setSegmentOuterRadius] = useState(250);
+    const [canvasWidth, setCanvasWidth] = useState(600);
+    const [canvasHeight, setCanvasHeight] = useState(600);
+    const [segmentAnglePadding, setSegmentAnglePadding] = useState(0.02);
+    const [precision, setPrecision] = useState(1);
 
-  const color_palette = d3.scaleSequential(d3.interpolateViridis);
-  const tick_precision = 200000;
+    const colorPalette = d3.scaleSequential(d3.interpolateViridis);
 
-
-  const handleFileUpload = (file: File) => {
-    const reader = new FileReader();
-    reader.onload = (event) => {
-      try {
-        const json = JSON.parse(event.target?.result as string);
-        setSegments(json);
-      } catch (error) {
-        console.error('Invalid JSON file:', error);
-        alert('Uploaded file is not valid JSON.');
-      }
-    };
-    reader.readAsText(file);
-  };
-
-  const updateAssemblyConfig = (
-    segmentPadding: number,
-    axisLabelFontSize: number,
-    showAxis: boolean, 
-    segmentInnerRadius: number, 
-    segmentOuterRadius: number,
-    segmentGridPadding: number,
-    showAxisLabels: boolean,
-    canvasWidth: number,
-    canvasHeight: number,
-    tickLength: number,
-    tickTextPadding: number
-  ) => {
-    setSegmentAnglePadding(segmentPadding);
-    setAxisLabelFontSize(axisLabelFontSize);
-    setShowAxis(showAxis);
-    setSegmentInnerRadius(segmentInnerRadius);
-    setSegmentOuterRadius(segmentOuterRadius);
-    setSegmentGridPadding(segmentGridPadding);
-    setShowAxisLabels(showAxisLabels);
-    setCanvasWidth(canvasWidth);
-    setCanvasHeight(canvasHeight);
-    setTickLength(tickLength);
-    setTickTextPadding(tickTextPadding);
-  };
-
-  useEffect(() => {
-    if (canvasRef.current && segments.length > 0) {
-      const total_length = segments.reduce((acc, segment) => acc + (segment.end - segment.start), 0);
-      const total_angle_padding = 2 * segmentAnglePadding * segments.length;
-      const total_available_angle = 2 * Math.PI - total_angle_padding;
-
-      d3.select(canvasRef.current).select("svg").remove();
-
-      const svg = d3
-        .select(canvasRef.current)
-        .append("svg")
-        .attr("viewBox", [0, 0, canvasWidth, canvasHeight])
-        .attr("width", canvasWidth)
-        .attr("height", canvasHeight)
-        .style("border", "1px solid black");
-
-      let last_angle = 0;
-
-      segments.forEach((segment, i) => {
-        const startAngle = last_angle + segmentAnglePadding;
-        const endAngle = last_angle + segmentAnglePadding + ((segment.end - segment.start) / total_length) * total_available_angle;
-
-        const arc = d3.arc()
-          .innerRadius(segmentInnerRadius)
-          .outerRadius(segmentOuterRadius)
-          .startAngle(startAngle)
-          .endAngle(endAngle);
-
-        svg.append("path")
-          .attr("d", arc)
-          .attr("transform", `translate(${canvasWidth / 2}, ${canvasHeight / 2})`)
-          .attr("fill", color_palette(0.55 * i/segments.length))
-          .on("mouseover", function (this: SVGPathElement) {
-            d3.select(this).attr("filter", "brightness(0.9)");
-          })
-          .on("mouseout", function (this: SVGPathElement) {
-            d3.select(this).attr("filter", "brightness(1)");
-          });
-        
-          svg.append("text")
-          .attr("x", canvasWidth / 2)
-          .attr("y", canvasHeight / 2)
-          .attr("text-anchor", "middle")
-          .attr("dominant-baseline", "central")
-          .attr("font-size", 12)
-          .attr("transform", `translate(${(segmentInnerRadius + segmentOuterRadius) / 2 * Math.sin((startAngle + endAngle) / 2)}, ${-(segmentInnerRadius + segmentOuterRadius) / 2 * Math.cos((startAngle + endAngle) / 2)})`)
-          .attr("fill", "white")
-          .text(segment.chromosome);
-
-        if (showAxis) {
-            const arc_grid = d3.arc()
-                .innerRadius(segmentOuterRadius + segmentGridPadding)
-                .outerRadius(segmentOuterRadius + segmentGridPadding)
-                .startAngle(startAngle)
-                .endAngle(endAngle);
-        
-            svg.append("path")
-                .attr("d", arc_grid)
-                .attr("transform", `translate(${canvasWidth / 2}, ${canvasHeight / 2})`)
-                .attr("stroke", "black")
-                .attr("stroke-width", 1);
-            
-            const numTicks = Math.floor((segment.end - segment.start) / tick_precision);
-            const scale = d3.scaleLinear()
-            .domain([segment.start, segment.end])
-            .range([startAngle, endAngle]);
-
-            const ticks = scale.ticks(numTicks);
-
-            ticks.forEach((tick: any) => {
-                const angle = scale(tick);
-                svg
-                    .append("line")
-                    .attr("x1", (canvasWidth / 2) + (segmentOuterRadius + segmentGridPadding) * Math.sin(angle))
-                    .attr("y1", (canvasHeight / 2) - (segmentOuterRadius + segmentGridPadding) * Math.cos(angle))
-                    .attr("x2", (canvasWidth / 2) + (segmentOuterRadius + segmentGridPadding + tickLength) * Math.sin(angle))
-                    .attr("y2", (canvasHeight / 2) - (segmentOuterRadius + segmentGridPadding + tickLength) * Math.cos(angle))
-                    .attr("stroke", "black")
-                    .attr("stroke-width", 1);
-                
-                const x = (canvasWidth / 2) + (segmentOuterRadius + segmentGridPadding + tickLength + tickTextPadding) * Math.sin(angle);
-                const y = (canvasHeight / 2) - (segmentOuterRadius + segmentGridPadding + tickLength + tickTextPadding) * Math.cos(angle);
-                const rotation = ((angle * 180) / Math.PI) - 90;
-                const tick_text = tick == 0 ? "" : tick.toLocaleString();
-                // `${rotation > 90 ? -rotation : rotation}`;
-                svg
-                    .append("text")
-                    .attr("x", x)
-                    .attr("y", y)
-                    .attr("text-anchor", `${rotation >= 90 ? "end" : "start"}`)
-                    .attr("dominant-baseline", "central")
-                    .attr("font-size", axisLabelFontSize)
-                    .attr("transform", `rotate(${rotation >= 90 ? rotation - 180 : rotation}, ${x}, ${y})`)
-                    .text(tick_text);
-            });
+    const handleFileUpload = (file: File) => {
+        const reader = new FileReader();
+        reader.onload = (event) => {
+        try {
+            const json = JSON.parse(event.target?.result as string);
+            setSegments(json);
+        } catch (error) {
+            console.error("Invalid JSON file:", error);
+            alert("Uploaded file is not valid JSON.");
         }
+        };
+        reader.readAsText(file);
+    };
 
-        last_angle = endAngle + segmentAnglePadding;
-      });
-    }
-  }, [
-    segments, 
-    segmentAnglePadding, 
-    axisLabelFontSize, 
-    showAxis, 
-    segmentInnerRadius, 
-    segmentOuterRadius, 
-    segmentGridPadding, 
-    showAxisLabels, 
-    canvasWidth, 
-    canvasHeight,
-    tickLength,
-    tickTextPadding
-]);
+    useEffect(() => {
+        if (canvasRef.current && segments.length > 0) {
+            d3.select(canvasRef.current).select("svg").remove();
 
-  return (
-    <div>
-      <h1>D3 Canvas Page</h1>
-      <FileUpload onFileUpload={handleFileUpload} />
-      <div ref={canvasRef}></div>
-      {segments.length > 0 ? <Form onUpdate={updateAssemblyConfig}/> : null}
-    </div>
-  );
+            const totalLength = segments.reduce(
+                (acc, segment) => acc + (segment.end - segment.start),
+                0
+            );
+
+            const svg = d3
+                .select(canvasRef.current)
+                .append("svg")
+                .attr("viewBox", [-canvasWidth / 2, -canvasHeight / 2, canvasWidth, canvasHeight])
+                .attr("width", canvasWidth)
+                .attr("height", canvasHeight)
+                .attr("style", "font: 10px sans-serif;");
+
+            const chord = d3.chord()
+                .padAngle(segmentAnglePadding)
+                .sortSubgroups(d3.descending);
+
+            const arc = d3.arc()
+                .innerRadius(segmentInnerRadius)
+                .outerRadius(segmentOuterRadius);
+
+            const axisGridlineArc = d3.arc()
+                .innerRadius(segmentOuterRadius + segmentGridPadding)
+                .outerRadius(segmentOuterRadius + segmentGridPadding);
+
+            const matrix = segments.map((segment, i) =>
+                segments.map((_, j) => (i === j ? segment.end - segment.start : 0))
+            );
+
+            const chords = chord(matrix);
+
+            const group = svg
+                .append("g")
+                .selectAll("g")
+                .data(chords.groups)
+                .join("g");
+
+            group
+                .append("path")
+                .attr("fill", (d: { index: number; }) => colorPalette(0.3 + 0.6 * d.index / segments.length))
+                .attr("d", arc)
+                .on("mouseover", (event: any, d: { index: number; }) => {
+                    d3.select(event.currentTarget).attr("filter", "brightness(0.95)");
+                    }
+                )
+                .on("mouseout", (event: any, d: { index: number; }) => {
+                    d3.select(event.currentTarget).attr("filter", "brightness(1)");
+                    }
+                )
+                .append("title")
+
+            group
+                .append("text")
+                .attr("x", 0)
+                .attr("y", 0)
+                .attr("transform", (d: { startAngle: number; endAngle: number; }) => {
+                    const angle = (d.startAngle + d.endAngle) / 2;
+                    const x = Math.sin(angle) * (segmentInnerRadius + (segmentOuterRadius - segmentInnerRadius) / 2);
+                    const y = -Math.cos(angle) * (segmentInnerRadius + (segmentOuterRadius - segmentInnerRadius) / 2);
+                    return `translate(${x}, ${y})`;
+                })
+                .attr("text-anchor", "middle")
+                .attr("alignment-baseline", "middle")
+                .attr("font-size", `10`)
+                .attr("fill", "white")
+                .text((d: { index: number; }) => segments[d.index].chromosome);
+
+            const precision_vals = [100, 250, 500]
+            const standardIntervalSizes = [500, 1000, 2500, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000]
+            const expectedTickPrecision = totalLength / precision_vals[precision];
+            const minorTickInterval = d3.bisect(standardIntervalSizes, expectedTickPrecision) === 0 ? standardIntervalSizes[0] : standardIntervalSizes[d3.bisect(standardIntervalSizes, expectedTickPrecision) - 1];
+            const tickInterval = minorTickInterval * 5;
+
+            if (showAxis) {
+                group
+                    .append("path")
+                    .attr("d", axisGridlineArc)
+                    .attr("stroke", "black")
+
+                group
+                    .append("g")
+                    .selectAll("g")
+                    .data((d: { index: string | number; startAngle: any; endAngle: any; }) => {
+                    const segment = segments[d.index];
+                    const scale = d3.scaleLinear()
+                        .domain([segment.start, segment.end])
+                        .range([d.startAngle, d.endAngle]);
+
+                    const majorTicks = d3.range(segment.start, segment.end, tickInterval).map((value: number) => ({
+                        value: value - segment.start == 0 ? null : d3.formatPrefix(",.0", 1e3)(value),
+                        angle: scale(value),
+                        isMajor: true,
+                    }));
+
+                    const minorTicks = d3.range(segment.start, segment.end, minorTickInterval).filter((value: number) => value % tickInterval !== 0).map((value: any) => ({
+                        value: null,
+                        angle: scale(value),
+                        isMajor: false,
+                    }));
+
+                    return [...majorTicks, ...minorTicks];
+                    })
+                    .join("g")
+                    .attr("transform", (d: { angle: number; }) => `rotate(${(d.angle * 180) / Math.PI - 90}) translate(${segmentOuterRadius + segmentGridPadding}, 0)`)
+                    .call((g) =>
+                    g
+                        .append("line")
+                        .attr("stroke", "currentColor")
+                        .attr("x2", (d: { isMajor: any; }) => (d.isMajor ? tickLength * 2 : tickLength))
+                    )
+                    .call((g) =>
+                    g
+                        .filter((d: { isMajor: any; }) => d.isMajor)
+                        .append("text")
+                        .attr("x", 2 * tickLength + tickTextPadding)
+                        .attr("dy", "0.35em")
+                        .attr("font-size", `${axisLabelFontSize}`)
+                        .attr("transform", (d: { angle: number; }) =>
+                        d.angle > Math.PI ? `rotate(180) translate(-${4 * tickLength + 2 * tickTextPadding})` : null
+                        )
+                        .attr("text-anchor", (d: { angle: number; }) => (d.angle > Math.PI ? "end" : null))
+                        .text((d: { value: { toLocaleString: () => any; }; }) => d.value?.toLocaleString())
+                    );
+            }
+        }
+    }, [segments, segmentInnerRadius, segmentOuterRadius, segmentGridPadding, showAxis, axisLabelFontSize, tickLength, tickTextPadding, canvasWidth, canvasHeight, segmentAnglePadding, precision]);
+
+    return (
+        <div>
+        <h1>D3 Chord Diagram</h1>
+        <FileUpload onFileUpload={handleFileUpload} />
+        <div className="svg-container" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+        <div ref={canvasRef} style={{ border: "1px solid black" }}></div>
+        </div>
+        {segments.length > 0 && 
+            <Form onUpdate={
+                (
+                    segmentPadding, 
+                    axisLabelFontSize, 
+                    showAxis, 
+                    segmentInnerRadius, 
+                    segmentOuterRadius, 
+                    segmentGridPadding, 
+                    canvasWidth,
+                    canvasHeight,
+                    tickLength,
+                    tickTextPadding,
+                    precision
+                ) => {
+                setSegmentAnglePadding(segmentPadding);
+                setAxisLabelFontSize(axisLabelFontSize);
+                setShowAxis(showAxis);
+                setSegmentInnerRadius(segmentInnerRadius);
+                setSegmentOuterRadius(segmentOuterRadius);
+                setSegmentGridPadding(segmentGridPadding);
+                setCanvasWidth(canvasWidth);
+                setCanvasHeight(canvasHeight);
+                setTickLength(tickLength);
+                setTickTextPadding(tickTextPadding);
+                setPrecision(precision);
+            }} />
+        }
+        </div>
+    );
 };
 
 export default CanvasPage;
--- web/components/Form.tsx
+++ web/components/Form.tsx
@@ -8,24 +8,24 @@ export default function AssemblyForm({ onUpdate }: {
         segmentInnerRadius: number,
         segmentOuterRadius: number,
         segmentGridPadding: number,
-        showAxisLabels: boolean,
         canvasWidth: number,
         canvasHeight: number,
         tickLength: number,
-        tickTextPadding: number
+        tickTextPadding: number,
+        precision: number
     ) => void 
 }) {
   const [segmentPadding, setSegmentPadding] = useState(0.02);
-  const [axisLabelFontSize, setAxisLabelFontSize] = useState(6);
+  const [axisLabelFontSize, setAxisLabelFontSize] = useState(10);
   const [showAxis, setShowAxis] = useState(true);
   const [segmentInnerRadius, setSegmentInnerRadius] = useState(200);
   const [segmentOuterRadius, setSegmentOuterRadius] = useState(250);
   const [segmentGridPadding, setSegmentGridPadding] = useState(3);
-  const [showAxisLabels, setShowAxisLabels] = useState(true);
-  const [canvasWidth, setCanvasWidth] = useState(800);
+  const [canvasWidth, setCanvasWidth] = useState(600);
   const [canvasHeight, setCanvasHeight] = useState(600);
   const [tickLength, setTickLength] = useState(3);
   const [tickTextPadding, setTickTextPadding] = useState(3);
+  const [precision, setPrecision] = useState(1);
 
     const onFormUpdate = () => {
         if (segmentInnerRadius >= segmentOuterRadius) {
@@ -34,7 +34,7 @@ export default function AssemblyForm({ onUpdate }: {
             setSegmentOuterRadius(250);
             return;
         }
-        onUpdate(segmentPadding, axisLabelFontSize, showAxis, segmentInnerRadius, segmentOuterRadius, segmentGridPadding, showAxisLabels, canvasWidth, canvasHeight, tickLength, tickTextPadding);
+        onUpdate(segmentPadding, axisLabelFontSize, showAxis, segmentInnerRadius, segmentOuterRadius, segmentGridPadding, canvasWidth, canvasHeight, tickLength, tickTextPadding, precision);
     };
 
   return (
@@ -46,7 +46,7 @@ export default function AssemblyForm({ onUpdate }: {
             <br />
             <label>
                 Axis Label Font Size</label>
-                <input type="range" min="0" max="10" step="1" value={axisLabelFontSize} onChange={(e) => setAxisLabelFontSize(parseFloat(e.target.value))} />
+                <input type="range" min="6" max="16" step="1" value={axisLabelFontSize} onChange={(e) => setAxisLabelFontSize(parseFloat(e.target.value))} />
             <br />
             <label>
                 Show Axis
@@ -65,10 +65,6 @@ export default function AssemblyForm({ onUpdate }: {
                 Segment Grid Padding</label>
                 <input type="range" min="1" max="5" step="1" value={segmentGridPadding} onChange={(e) => setSegmentGridPadding(parseFloat(e.target.value))} />
             <br />
-            <label>
-                Show Axis Labels
-                <input type="checkbox" checked={showAxisLabels} onChange={(e) => setShowAxisLabels(e.target.checked)} />
-            </label>
             <br />
             <label>
                 Canvas Width</label>
@@ -85,6 +81,10 @@ export default function AssemblyForm({ onUpdate }: {
             <label>
                 Tick Text Padding</label>
                 <input type="range" min="0" max="10" step="1" value={tickTextPadding} onChange={(e) => setTickTextPadding(parseFloat(e.target.value))} />
+            <br />
+            <label>
+                Precision</label>
+                <input type="range" min="0" max="2" step="1" value={precision} onChange={(e) => setPrecision(parseFloat(e.target.value))} />
             <br />
         </form>
       <button onClick={onFormUpdate}>Update</button>
