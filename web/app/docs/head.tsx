b93316017559de233e6cee4660a69d10be3cb765
Mithun Srinavaratnarajah <lnsmirth@googlemail.com>
2025-05-27 15:26:35 +0100

    [Hotfix] Resolve error with docs

---
+++ web/app/docs/head.tsx
@@ -0,0 +1,15 @@
+// app/docs/head.tsx
+export default function Head() {
+    return (
+      <>
+        <title>Project Documentation</title>
+        <meta name="description" content="Docs powered by Docsify" />
+        {/* Docsify CSS theme */}
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/docsify/lib/themes/vue.css"
+        />
+      </>
+    );
+  }
+  
